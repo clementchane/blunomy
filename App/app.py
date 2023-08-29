ed5e796658f96d050502764e8bd774ac9d89f69d
Clement Ching <clement.ching@zetaris.com>
2023-08-29 13:16:15 +1000

    add gitignore and fix pyapp

--- App/app.py
+++ App/app.py
@@ -4,12 +4,11 @@ app = Flask(__name__)
 
 @app.route('/returnjson', methods = ['GET'])
 def ReturnJSON():
-	if(request.method == 'GET'):
-		data = {
-			"Hello" : "World"
-		}
-
-		return jsonify(data)
+	data = request.get_json()
+	numbers = data.get('numbers', [])
+	total_sum = sum(numbers)
+    
+	return jsonify({'sum': total_sum}), 200
 
 if __name__=='__main__':
-	app.run(debug=True)
+    app.run(host='0.0.0.0')
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # blunomy
+
+Add sample app and end to end pipeline to deploy this app into an AKS cluster
